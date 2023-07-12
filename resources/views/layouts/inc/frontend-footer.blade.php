5c39f6b6512f9064c039808cbe48ba54a1a9194b
Hiba <hhahsh2000@gmail.com>
2023-07-12 22:49:46 +0200

    blog project

---
+++ resources/views/layouts/inc/frontend-footer.blade.php
@@ -0,0 +1,39 @@
+<div class="py-5 bg-dark text-white">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h5>Tadween Blog</h5>
+                <div class="underline"></div>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias odit consequatur incidunt ducimus autem nihil nam beatae et voluptates dolores possimus, architecto accusamus ipsum obcaecati doloremque provident, magnam inventore voluptatibus.
+                </p>
+            </div>
+            <div class="col-md-3">
+                <h5>Quick Links</h5>
+                <div class="underline"></div>
+                <div><a href="" class="text-white">Home</a></div>
+                <div><a href="" class="text-white">About Us</a></div>
+                <div><a href="" class="text-white">Contact Us</a></div>
+                <div><a href="" class="text-white">Needs Promotion ?</a></div>
+            </div>
+            <div class="col-md-3">
+                <h5>Follow Us On</h5>
+                <div class="underline"></div>
+                <div><a href="https://www.facebook.com" class="text-white">Facebook</a></div>
+                <div><a href="https://www.instagram.com" class="text-white">Instagram</a></div>
+                <div><a href="https://www.linkedin.com" class="text-white">Linkedin</a></div>
+                <div><a href="https://www.twitter.com" class="text-white">Twitter</a></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="py-2 bg-gray">
+    <div class="container text-center">
+        <p class="mb-0">
+            &copy; Copyright at Tadween.
+            All right reserved.
+            Designed and Developed by Tadween team {{ date(('d-m-Y')) }}.
+        </p>
+    </div>
+</div>
+
